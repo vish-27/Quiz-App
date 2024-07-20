0620e2795fb1f808b5c9269a9518b34612ac6eb6
Vishakha Agrawal <agrawalvishakha23@gmail.com>
2024-07-20 18:24:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Quizz from './Components/Quizz/Quizz'
+
+const App = ()=> {
+  return (
+    <div>
+        <Quizz />
+    </div>
+  )
+}
+
+export default App
